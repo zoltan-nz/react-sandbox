2b4e24b93185514b83a79e83c5e978925fc189c4
Zoltan <zdebre@gmail.com>
2020-06-01 23:58:00 +1200

    Add basic form with useState hook

---
+++ src/Accounts.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+interface Account {
+  id: number;
+  name: string;
+  selected?: boolean;
+}
+
+interface Balance {
+  amount?: number;
+  currency?: string;
+}
+
+const Accounts = () => {
+  const [accounts, setAccounts] = useState<Account[]>([
+    { id: 1, name: 'First', selected: false },
+    { id: 2, name: 'Second', selected: false },
+  ]);
+
+  const [balance, setBalance] = useState<Balance>({ amount: 0, currency: 'NZD' });
+
+  return (
+    <>
+      <ul>
+        {accounts.map((account) => (
+          <li key={account.id}>
+            {account.id} {'=>'} {account.name} {'=>'} {account.selected ? 'yes' : 'no'}
+            <button
+              onClick={() => {
+                account.selected = !account.selected;
+                setAccounts([...accounts]);
+              }}
+            >
+              toggle
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        {balance?.amount} {balance?.currency}
+      </div>
+      <input value={balance.amount} onChange={(e) => setBalance({ ...balance, amount: parseInt(e.target.value) })} />{' '}
+      <input value={balance.currency} onChange={(e) => setBalance({ ...balance, currency: e.target.value })} />
+    </>
+  );
+};
+
+export default Accounts;
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Accounts from './Accounts';
 
-function App() {
-  return (
-    <div>
-      <header>
-        <h1>React Sandbox</h1>
-      </header>
+const Home = () => <h2>Home Page</h2>;
+const About = () => <h2>About Page</h2>;
+
+const App = () => (
+  <BrowserRouter>
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
+      <div className="container">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/" exact={true}>
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/accounts">
+              Accounts
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div className="container">
+      <Switch>
+        <Route path="/accounts">
+          <Accounts />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
-  );
-}
+  </BrowserRouter>
+);
 
 export default App;
