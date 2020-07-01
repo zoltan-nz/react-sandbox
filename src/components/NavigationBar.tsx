32005a6574f2f620176b47829b4e2601a01256d2
Zoltan <zdebre@gmail.com>
2020-07-02 09:09:00 +1200

    Add Grommet

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import { Box, Button, Heading } from 'grommet';
+import { Notification } from 'grommet-icons';
+import React, { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 
+const AppBar: FC = (props) => (
+  <Box
+    tag="header"
+    direction="row-responsive"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation="xsmall"
+    style={{ zIndex: 1 }}
+    {...props}
+  />
+);
+
 export const NavigationBar = () => (
-  <nav>
-    <NavLink className="nav-link" to="/" end={true}>
-      Home
-    </NavLink>{' '}
-    |{' '}
-    <NavLink className="nav-link" to="/products">
-      Products
-    </NavLink>{' '}
-    |{' '}
-    <NavLink className="nav-link" to="/about">
-      About
-    </NavLink>
-  </nav>
+  <AppBar>
+    <Heading level="3" margin="none">
+      My App
+    </Heading>
+    <nav>
+      <NavLink to="/" end={true}>
+        Home
+      </NavLink>{' '}
+      | <NavLink to="/products">Products</NavLink> | <NavLink to="/about">About</NavLink>
+      <Button icon={<Notification />} onClick={() => {}} />
+    </nav>
+  </AppBar>
 );
