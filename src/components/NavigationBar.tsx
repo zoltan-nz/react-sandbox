0d357cc9b2958693f9db327ffab8ea0458637e62
Zoltan <zdebre@gmail.com>
2020-08-24 02:08:00 +1200

    Building the navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -5,16 +5,16 @@ import { NavLink } from 'react-router-dom';
 export const NavigationBar = () => {
   return (
     <AppBar position="static" color="default">
-      <Container style={{ flexGrow: 1}}>
+      <Container style={{ flexGrow: 1 }}>
         <h1 style={{ flexGrow: 1 }}> Sandbox</h1>
         <nav style={{ display: 'flex', flexGrow: 1, justifyContent: 'flex-end' }}>
-          <NavLink style={{ flexGrow: 1 }} to='/'>
+          <NavLink style={{ flexGrow: 1 }} to="/">
             Home
           </NavLink>
-          <NavLink style={{ flexGrow: 1 }} to='/products'>
+          <NavLink style={{ flexGrow: 1 }} to="/products">
             Products
           </NavLink>
-          <NavLink style={{ flexGrow: 1 }} to='/about'>
+          <NavLink style={{ flexGrow: 1 }} to="/about">
             About
           </NavLink>
         </nav>
