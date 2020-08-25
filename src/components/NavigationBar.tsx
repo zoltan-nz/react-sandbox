38940b0c81f0fc5eee193a9197c2b73ad5df2ab8
Zoltan <zdebre@gmail.com>
2020-08-25 12:02:00 +1200

    Align navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,25 +1,31 @@
-import { AppBar, Container } from '@material-ui/core';
+import styled from '@emotion/styled';
+import { AppBar, Box, Button, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const MenuLink = styled(NavLink)`
+  color: inherit;
+  flex-grow: 1;
+
+  &.active {
+    color: red;
+  }
+`;
+
 export const NavigationBar = () => {
   return (
-    <AppBar position="static" color="default">
-      <Container style={{ flexGrow: 1 }}>
-        <h1 style={{ flexGrow: 1 }}> Sandbox</h1>
-        <nav style={{ display: 'flex', flexGrow: 1, justifyContent: 'flex-end' }}>
-          <NavLink style={{ flexGrow: 1 }} to="/">
-            Home
-          </NavLink>
-          <NavLink style={{ flexGrow: 1 }} to="/products">
-            Products
-          </NavLink>
-          <NavLink style={{ flexGrow: 1 }} to="/about">
-            About
-          </NavLink>
-        </nav>
-        <button style={{ flexGrow: 1 }}>Login</button>
-      </Container>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Sandbox
+          </Typography>
+          <MenuLink to="/">Home</MenuLink>
+          <MenuLink to="/products">Products</MenuLink>
+          <MenuLink to="/about">About</MenuLink>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
